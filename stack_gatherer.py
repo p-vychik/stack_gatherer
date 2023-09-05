bd27f88e2931dd1a49f029bb8e71d6e6f6642e2b
artgolden <artemiy.golden@protonmail.com>
2023-09-05 22:36:36 +0200

    Added full path parsing  to ImageFile

--- stack_gatherer.py
+++ stack_gatherer.py
@@ -17,9 +17,9 @@ active_stacks = {}
 class ImageFile:
     """
     File naming for light-sheet image files.
-    Example file name: TP-0001_SPC-0001_ILL-0_CAM-0_CH-01_PL-0001_.tif
-    :param file_name: full image file name
-    :type file_name: str
+    Example file name: TP-0001_SPC-0001_ILL-0_CAM-0_CH-01_PL-0001-outOf-0150_blaBla.tif
+    :param file_path: full path to image
+    :type file_path: str
     """
     time_point = 0
     specimen = 0
@@ -27,14 +27,18 @@ class ImageFile:
     camera = 0
     channel = 0
     plane = 0
+    total_num_planes = 0
     additional_info = ""
     extension = ""
+    path_to_image_dir = ""
 
-    def __init__(self, file_name):
-        split_by = "TP-|SPC-|ILL-|CAM-|CH-|PL-|\."
+    def __init__(self, file_path):
+        self.path_to_image_dir = os.path.dirname(file_path)
+        file_name = os.path.basename(file_path)
+        split_by = "TP-|SPC-|ILL-|CAM-|CH-|PL-|outOf-|\."
         name_parts = re.split(split_by, file_name)
         
-        if len(name_parts) == 8:
+        if len(name_parts) == 9:
             for i, name_part in enumerate(name_parts):
                 
                 if i == 0:
@@ -50,14 +54,16 @@ class ImageFile:
                 elif i == 5:
                     self.channel = int(name_part.strip("-_"))
                 elif i == 6:
+                    self.plane = int(name_part.strip("-_"))
+                elif i == 7:
                     name_and_info = name_part.strip("-_").split("_", 1)
                     if len(name_and_info) == 1:
-                        self.plane = int(plane.strip("-_"))
+                        self.total_num_planes = int(name_and_info.strip("-_"))
                     else:
-                        plane, info = name_and_info
-                        self.plane = int(plane.strip("-_"))
+                        num_planes, info = name_and_info
+                        self.total_num_planes = int(num_planes.strip("-_"))
                         self.additional_info = info
-                elif i == 7:
+                elif i == 8:
                     self.extension = name_part
         else:
             raise Exception(
@@ -74,7 +80,8 @@ class ImageFile:
             dataset_name = dataset_name + "_"
         return (f"{dataset_name}TP-{self.time_point:04}"
                 f"_SPC-{self.specimen:04}_ILL-{self.illumination}"
-                f"_CAM-{self.camera}_CH-{self.channel:02}_PL-{self.plane:04}{additional_info}.{self.extension}" 
+                f"_CAM-{self.camera}_CH-{self.channel:02}"
+                f"_PL-{self.plane:04}-outOf-{self.total_num_planes:04}{additional_info}.{self.extension}" 
                 )
 
     def get_name_without_extension(self):
@@ -83,15 +90,21 @@ class ImageFile:
             additional_info = "_" + additional_info
         return (f"{self.dataset_name}_TP-{self.time_point:04}"
                 f"_SPC-{self.specimen:04}_ILL-{self.illumination}"
-                f"_CAM-{self.camera}_CH-{self.channel:02}_PL-{self.plane:04}{additional_info}" 
+                f"_CAM-{self.camera}_CH-{self.channel:02}_PL-{self.plane:04}-outOf-{self.total_num_planes:04}{additional_info}" 
                 )
+    def get_file_path(self):
+        return os.path.join(self.path_to_image_dir, self.get_name())
 
 
 STOP_FILE_NAME = "STOP_STACK_GATHERING"
 
-def check_if_a_stack_is_ready(filename):
+def add_file_to_active_stacks(file_path):
+    file_obj = ImageFile(os.path.basename(file_path))
+    stack_signature = ()
+
+def check_if_a_stack_is_ready(file_path):
     # Replace this with your logic to check if a stack is ready
-    print(f"Checking if stack is ready for file: {filename}")
+    print(f"Checking if stack is ready for file: {file_path}")
 
 # Define the event handler class
 
@@ -100,8 +113,10 @@ class MyHandler(FileSystemEventHandler):
     def on_created(self, event):
         if event.is_directory:
             return
+        file_path =  event.src_path
         # Call the function when a new file is created
-        check_if_a_stack_is_ready(event.src_path)
+        add_file_to_active_stacks(file_path)
+        check_if_a_stack_is_ready(file_path)
 
 
 def main():
--- tests/test_ImageFile.py
+++ tests/test_ImageFile.py
@@ -6,16 +6,16 @@ sys.path.append('../stack_gatherer')
 from stack_gatherer import ImageFile
 
 def test_same_filename_is_returned():
-    input = "dataseTPtName_TP-0001_SPC-0001_ILL-0_CAM-0_CH-01_PL-0001_ffdfs.tif"
+    input = "dataseTPtName_TP-0001_SPC-0001_ILL-0_CAM-0_CH-01_PL-0001-outOf-0150_ffdfs.tif"
     assert input == ImageFile(input).get_name()
 
 def test_same_filename_withot_extension_is_returned():
-    input = "dataseTPtName_TP-0001_SPC-0001_ILL-0_CAM-0_CH-01_PL-0001_ffdfs.tif"
-    input_no_extension = "dataseTPtName_TP-0001_SPC-0001_ILL-0_CAM-0_CH-01_PL-0001_ffdfs"
+    input = "dataseTPtName_TP-0001_SPC-0001_ILL-0_CAM-0_CH-01_PL-0001-outOf-0150_ffdfs.tif"
+    input_no_extension = "dataseTPtName_TP-0001_SPC-0001_ILL-0_CAM-0_CH-01_PL-0001-outOf-0150_ffdfs"
     assert input_no_extension == ImageFile(input).get_name_without_extension()
 
 def test_params_values():
-    input = "dataseTPtName_TP-0001_SPC-0101_ILL-3_CAM-2_CH-11_PL-0777_ffdfs.tif"
+    input = r"foo/bar/dataseTPtName_TP-0001_SPC-0101_ILL-3_CAM-2_CH-11_PL-0777-outOf-1000_ffdfs.tif"
     obj = ImageFile(input)
     assert obj.dataset_name == "dataseTPtName"
     assert obj.time_point == 1
@@ -24,5 +24,11 @@ def test_params_values():
     assert obj.camera == 2
     assert obj.channel == 11
     assert obj.plane == 777
+    assert obj.total_num_planes == 1000
     assert obj.additional_info == "ffdfs"
     assert obj.extension == "tif"
+    assert obj.path_to_image_dir == r"foo/bar"
+
+def test_same_full_path_is_returned():
+    input = r"foo/bar/dataseTPtName_TP-0001_SPC-0001_ILL-0_CAM-0_CH-01_PL-0001-outOf-0150_ffdfs.tif"
+    assert input == ImageFile(input).get_file_path()
